caf8995e09e92ee66e38229782a072c78fc43175
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2022-05-10 23:24:10 -0400

    il2cpp uses base.Log instead of base.Logger

--- templates/BepInEx6.PluginTemplate.Il2Cpp/Plugin.cs
+++ templates/BepInEx6.PluginTemplate.Il2Cpp/Plugin.cs
@@ -7,12 +7,12 @@ namespace BepInEx6.PluginTemplate.Il2Cpp
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BasePlugin
     {
-        internal static ManualLogSource Log;
+        internal static new ManualLogSource Log;
 
         public override void Load()
         {
             // Plugin startup logic
-            Log = Logger;
+            Log = base.Log;
             Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
