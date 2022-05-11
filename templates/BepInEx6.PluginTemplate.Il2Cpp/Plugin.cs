2c726b896f5910caf944cbfc6f1d237badfe32e1
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2022-05-10 23:02:39 -0400

    add global logger to template

--- templates/BepInEx6.PluginTemplate.Il2Cpp/Plugin.cs
+++ templates/BepInEx6.PluginTemplate.Il2Cpp/Plugin.cs
@@ -1,14 +1,18 @@
 ﻿using BepInEx;
 using BepInEx.IL2CPP;
+using BepInEx.Logging;
 
 namespace BepInEx6.PluginTemplate.Il2Cpp
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BasePlugin
     {
+        internal static ManualLogSource Log;
+
         public override void Load()
         {
             // Plugin startup logic
+            Log = Logger;
             Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
--- templates/BepInEx6.PluginTemplate.NET/Plugin.cs
+++ templates/BepInEx6.PluginTemplate.NET/Plugin.cs
@@ -1,14 +1,18 @@
 ﻿using BepInEx;
 using BepInEx.NetLauncher;
+using BepInEx.Logging;
 
 namespace BepInEx6.PluginTemplate.NET
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BasePlugin
     {
+        internal static ManualLogSource Log;
+
         public override void Load()
         {
             // Plugin startup logic
+            Log = Logger;
             Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
