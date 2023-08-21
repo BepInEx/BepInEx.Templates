e5f2e667ef16450a6d77df7f65944d08bdb8cfcc
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-08-21 11:05:10 +0200

    Fix mono logger name

--- BepInEx.Templates/templates/BepInEx6.PluginTemplate.Unity.Mono/Plugin.cs
+++ BepInEx.Templates/templates/BepInEx6.PluginTemplate.Unity.Mono/Plugin.cs
@@ -6,12 +6,12 @@ namespace BepInEx6.PluginTemplate.Unity.Mono;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
-    internal static new ManualLogSource Log;
+    internal static new ManualLogSource Logger;
 
     public override void Load()
     {
         // Plugin startup logic
-        this.Log = base.Log;
+        this.Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
 }
