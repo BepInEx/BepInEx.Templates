b93d72bc5aacb61501ce6d05ad7edb0357a53342
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-08-21 10:55:39 +0200

    Update Plugin.cs

--- templates/BepInEx5.PluginTemplate/Plugin.cs
+++ templates/BepInEx5.PluginTemplate/Plugin.cs
@@ -6,13 +6,13 @@ namespace BepInEx5.PluginTemplate
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        internal static ManualLogSource Log;
+        internal static new ManualLogSource Logger;
         
         private void Awake()
         {
             // Plugin startup logic
-            Log = Logger;
-            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            this.Logger = Logger;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
 }
